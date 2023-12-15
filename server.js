dab03e21307466e5e67e1f6fcda4d8c5a807dc9e
Jakkiexplore <yangjakki@gmail.com>
2023-12-15 17:23:57 -0600

    scafolding server

--- server.js
+++ server.js
@@ -2,14 +2,21 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-
-
+const routes = require('./controllers');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 
 app.listen(PORT, () => {
