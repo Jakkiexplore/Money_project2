d19e09c9f3dfa02daa42d5586c34bdcee15140ee
dpkashrestha <dpkashrestha7@gmail.com>
2023-12-21 15:50:52 -0600

    Added add/delete functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
     // Set up sessions with a 'loggedIn' variable set to `true`
     req.session.save(() => {
       req.session.loggedIn = true;
-
+      req.session.userId = dbUserData.dataValues.id
       res.status(200).json(dbUserData);
     });
   } catch (err) {
@@ -38,7 +38,7 @@ router.post('/login', async (req, res) => {
         .json({ message: 'Incorrect username. Please try again!' });
       return;
     }
-    console.log(dbUserData)
+    
     const validPassword = await dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
@@ -51,7 +51,7 @@ router.post('/login', async (req, res) => {
     // Once the user successfully logs in, set up the sessions variable 'loggedIn'
     req.session.save(() => {
       req.session.loggedIn = true;
-      req.session.userid = dbUserData.dataValues.id
+      req.session.userId = dbUserData.dataValues.id
       req.session.username = req.body.username
       res
         .status(200)
